bdf231cf66e4e2099a5b01fbac0004a3644f92b5
asm95 <crissonicx2@gmail.com>
2019-05-05 17:43:16 -0300

    outline on how scheduler will spawn children

--- fork.c
+++ fork.c
@@ -19,7 +19,7 @@ void print_path(int *arr, int sz){
 }
 
 void child_manager(to_proxy *topo){
-    printf("(I) Child process\n");
+    printf("(I) Initiated %d process\n", getpid());
 
     int *path, path_sz;
     printf("(I) Path from 0 to A using HyperCube is: ");
@@ -27,37 +27,60 @@ void child_manager(to_proxy *topo){
     print_path(path, path_sz);
 }
 
-void parent_manager(to_proxy *topo){
-    printf("(I) Parent process\n");
+#define NRO_PROC 3
+
+void parent_manager(to_proxy *topo, int *pid_vec){
+    printf("(I) Parent process (PID: %d)\n", pid_vec[0]);
 
     int *path, path_sz;
     printf("(I) Path from 7 to 9 using HyperCube is: ");
     path = topology_query(topo, 0x7, 0x9, &path_sz);
     print_path(path, path_sz);
+
+    // shutdown process
+    // breaks topology but kills the current executing processes
+    int status;
+    for (int id=1; id < NRO_PROC; id++){
+        // don't forget to do a waitpid on fork cus your process will turn into a zombie
+        // and zombies don't release their line in the process table!
+        // you can do a waitpid after the process exited
+        waitpid(pid_vec[id], &status, 0);
+    }
 }
 
 void test_spawn_processes(){
     printf("(I) Gerenciador de Processos\n");
+    printf("(I) Número de processos a serem criados: %d\n", NRO_PROC-1);
 
     to_proxy *tp = topology_create(HYPER_C);
-    topology_clear(tp);
+    
 
-    int pid = fork();
-    int status;
+    int pid_vec[NRO_PROC];
+
+    int pid;
+    // 0 is the master node itself
+    for (int id = 1; id < NRO_PROC; id++){
+        pid = fork();
+        if (pid == 0){
+            // child will break the for to avoid creating more processes
+            break;
+        } else {
+            // parent will fill the pid table
+            pid_vec[id] = pid;
+        }
+    }
+
     if (pid == 0){
         child_manager(tp);
     } else {
-        parent_manager(tp);
-
-        // don't forget to do a waitpid on fork cus your process will turn into a zombie
-        // and zombies don't release their line in the process table!
-        // you can do a waitpid after the process exited
-
-        waitpid(pid, &status, 0);
+        pid_vec[0] = getpid();
+        parent_manager(tp, pid_vec);
     }
+
+    topology_clear(tp);
 }
 
 int main(){
-    test_msg_queue();
+    test_spawn_processes();
     return 0;
 }
