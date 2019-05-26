b6666160d1de71f67fab09ab17c749d38abde5cf
asm95 <crissonicx2@gmail.com>
2019-05-26 14:22:18 -0300

    [deps] route_walk is used by children nodes
    Exported route_walk function definition to master.h because
    it is also used by children.
    
    Suggestion is to (maybe) move it to topology.h or create a new file that
    has shared code between master and
    slave modules.

--- sch/master.h
+++ sch/master.h
@@ -1,4 +1,6 @@
+#include "../msg/msg.h" // for msg_packet
 #include "../topology.h"
 
-void shutdown(int pid_vec[]);
-void parent_manager(to_proxy *topo, int *pid_vec);
+void    shutdown(int pid_vec[]);
+void    parent_manager(to_proxy *topo, int *pid_vec);
+int     route_walk(msg_packet *p); // required by children nodes
